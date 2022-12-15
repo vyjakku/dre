01de14697da579f367c049f5503b73d6d1673abe
vishnu <vishnujakku1912@gmail.com>
2022-12-15 13:48:38 +0530

    H

---
+++ StudentDetails.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class StudentDetails
+{
+	
+	{
+		String name;
+		int age,weight;
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the name of the student");
+		name=sc.nextLine();
+		System.out.println("Enter the age of the student");
+		age=sc.nextInt();
+		System.out.println("Enter the weight of the student");
+		weight=sc.nextInt();
+		System.out.println("name of the student is"+name);
+		System.out.println("age of the student is"+age);
+		System.out.println("weight of the student is"+weight);
+	}
+}
+		
+		
